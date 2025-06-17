d649249b246c54cc0efb731ffee0cd2491e284c4
irisHanb <fortrulymylife@gmail.com>
2025-06-17 22:45:29 +0900

    feat: create studio procedures, video create

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div>studio page</div>;
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export default async function Page() {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
